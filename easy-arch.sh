87ca296eb672735e09209b20203de23962ac4c31
Krait <mihneacreanga@gmail.com>
2024-01-20 02:11:09 +0100

    transfer

--- easy-arch.sh
+++ easy-arch.sh
@@ -334,8 +334,7 @@ mount "$BTRFS" /mnt
 
 # Creating BTRFS subvolumes.
 info_print "Creating BTRFS subvolumes."
-# subvols=(snapshots var_pkgs var_log home opt root srv tmp)
-subvols=(.snapshots var_pkgs var_log home root srv)
+subvols=(snapshots var_pkgs var_log home opt root srv tmp)
 for subvol in '' "${subvols[@]}"; do
     btrfs su cr /mnt/@"$subvol" &>/dev/null
 done
@@ -346,7 +345,7 @@ info_print "Mounting the newly created subvolumes."
 mountopts="ssd,noatime,compress-force=zstd:3,discard=async"
 mount -o "$mountopts",subvol=@ "$BTRFS" /mnt
 # mkdir -p /mnt/{home,opt,root,srv,.snapshots,var/{log,cache/pacman/pkg},boot}
-mkdir -p /mnt/{home,root,srv,.snapshots,var/{log,cache/pacman/pkg},boot}
+mkdir -p /mnt/{home,opt,root,srv,.snapshots,var/{log,cache/pacman/pkg},boot}
 for subvol in "${subvols[@]:2}"; do
     mount -o "$mountopts",subvol=@"$subvol" "$BTRFS" /mnt/"${subvol//_//}"
 done
